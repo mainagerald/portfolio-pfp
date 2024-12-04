afbc2857143b32cfcb06610f04048ff95c9cdca0
Flavian Maina <FLAVIAN.WAMBETI@equitybank.co.ke>
2024-12-04 14:32:30 +0300

    home-about

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Card from '@mui/material/Card';
+import { CardContent, CardHeader } from '@mui/material';
+
+const CustomCard = ({icon, title, description}) => {
+  return (
+    <Card variant='outlined' style={{boxShadow:'3px', borderRadius:'10px'}}>
+        <div className='flex justify-center p-1 m-1'>{icon}</div>
+        <CardContent>
+            <h2 className='font-bold text-red-400 m-1 p-1'>{title}</h2>
+            <p className='text-sm text-pretty'>{description}</p>
+        </CardContent>
+    </Card>
+  )
+}
+
+export default CustomCard
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,43 @@
 import React, { useEffect, useState } from 'react'
 import ProfessionalIdentity from '../components/ProfesionalIdentity';
+import { HiAcademicCap, HiArrowRight, HiDocumentText, HiLightningBolt, HiOutlineDesktopComputer } from 'react-icons/hi';
+import { Link } from 'lucide-react';
+import { FiArrowRight, FiCloudLightning } from 'react-icons/fi';
+import { useNavigate } from 'react-router-dom';
+import { HiBuildingLibrary, HiCodeBracket, HiPaintBrush } from 'react-icons/hi2';
+import Card from '../components/Card';
+import CustomCard from '../components/Card';
+import { MdEditNote, MdHandyman, MdUmbrella } from 'react-icons/md';
+import { IoMdUmbrella } from 'react-icons/io';
+import { BsLightningCharge } from 'react-icons/bs';
 
 const Home = () => {
   const [greeting, setGreeting] = useState('');
   const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate();
 
+  const tasksArray = [
+    {
+        title: 'Design', 
+        description: 'Architect comprehensive system schemas and API interfaces, optimizing application performance and creating intuitive frontend designs that elevate user experience and engagement.', 
+        icon: <HiPaintBrush size={50} className='text-violet-400'/>
+    },
+    {
+        title: 'Build', 
+        description: 'Develop robust applications with a strategic focus on performance optimization, advanced security protocols, and enterprise-grade fault tolerance, maintaining pristine and scalable code architecture.', 
+        icon: <MdHandyman size={50} className='text-amber-400'/>
+    },
+    {
+        title: 'Document', 
+        description: 'Maintain comprehensive technical documentation spanning the entire application lifecycle, ensuring clear, maintainable, and knowledge-transfer-friendly resources for long-term project sustainability.', 
+        icon: <MdEditNote size={50} className='text-teal-700'/>
+    },
+    {
+        title: 'Own', 
+        description: 'Engage comprehensively in the Software Development Life Cycle, implementing best practices, driving cross-functional collaboration, and ensuring end-to-end project excellence and operational productivity.', 
+        icon: <IoMdUmbrella size={50} className='text-cyan-400'/>
+    }
+]
   const greetings = [
     'Hello',
     'Konnichiwa',
@@ -64,7 +97,7 @@ const Home = () => {
         <ProfessionalIdentity />
       </div>
       <div className='text-center p-4 m-2'>
-        <div className='flex flex-col items-center space-y-4'>
+        <div className='flex flex-col items-center space-y-4 border-b-2 border-gray-300 p-2'>
           <h1 className='text-7xl font-bold text-gray-800 tracking-tight'>
             SCALABLE  SYSTEMS
           </h1>
@@ -75,11 +108,37 @@ const Home = () => {
             <h1 className='text-7xl font-bold text-gray-800 tracking-tight'>
               ENGINEER
             </h1>
-            <p className='text-sm text-gray-600 italic'>
+            <p className='text-gray-600 italic'>
               Based in Nairobi, Kenya
             </p>
           </div>
         </div>
+        <div className='fade-in mt-2 p-3 flex justify-between gap-4 w-1/2 text-start text-balance text-lg'>
+          <p className='font-lato'> I’m a passionate Software Engineer currently driving innovation in payment systems at Equity Group Holdings. 
+            My ethic is fueled by curiosity and a desire to create impactful and bespoke solutions.
+            My passions lie in distributed systems, robust application performance, optimization, and security throughout the software development cycle, 
+            while constantly seeking to expand my expertise in emerging technologies that shape the future of digital productivity.
+          </p>
+        </div>
+      <div className='fade-in flex justify-end'>
+      <button onClick={()=>navigate('/about')} className='rounded-lg flex flex-row font-lato text-lg font-semibold shadow-md p-2 m-2 hover:cursor-pointer hover:bg-gray-100'>
+            About Me <FiArrowRight size={25}/>
+          </button>
+      </div>
+      <div className='m-2 mt-3 p-2 border-t-2 border-gray-300'>
+        <p className='text-center text-xl font-bold text-blue-500'>What I Do...</p>
+      </div>
+      <div className='grid grid-cols-1 sm:grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-4'>
+        {tasksArray.map((task)=>(
+          <CustomCard key={task.title} icon={task.icon} title={task.title} description={task.description} />
+        ))}
+      </div>
+      <div className='m-2 p-2'>
+        <h1 className='font-bold font-lato text-2xl text-green-500 flex-row flex gap-2 justify-center p-2'>My Projects<BsLightningCharge size={30}/></h1>
+        <p>Project 1</p>
+        <p>Project 2</p>
+        <p>Project 3</p>
+      </div>
       </div>
     </div>
   )
