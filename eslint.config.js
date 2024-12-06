81e6894451bc265fc6b3802358a99cbd7f18d901
Flavian Maina <FLAVIAN.WAMBETI@equitybank.co.ke>
2024-12-06 11:54:26 +0300

    lint

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,17 @@ export default [
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
     languageOptions: {
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      react: pluginReact,
     },
     settings: {
       react: {
@@ -16,10 +26,12 @@ export default [
     },
     rules: {
       ...pluginJs.configs.recommended.rules,
-      ...pluginReact.configs.flat.recommended.rules,
+      ...pluginReact.configs.recommended.rules,
+      'react/prop-types': 'off',
       "react/react-in-jsx-scope": "off", // Disable for React 17+
       "no-unused-vars": "warn",         // Warn about unused variables
-      "react/no-unescaped-entities": "error", // Ensure JSX entities are escaped
+      "react/no-unescaped-entities": "off", // Ensure JSX entities are escaped
+      "react/display-name": "off",      // Optional: turn off display-name rule
     },
   },
 ];
